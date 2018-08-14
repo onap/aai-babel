9b8444de650689fa1361e510a07c252fdc727162
Lee, Tian (tl5884) <TianL@amdocs.com>
2018-08-14 11:43:17 +0100

    Fix Babel build issue
    Caused by a change in the structure of the aai-schema jar that Babel
    depends on.
    
    Change-Id: I68575ba0c727826de9a930a5e85c4f077d1127d1
    Issue-ID: AAI-1449
    Signed-off-by: Lee, Tian (tl5884) <TianL@amdocs.com>

--- scripts/get-latest-xsd-version.sh
+++ scripts/get-latest-xsd-version.sh
@@ -1,7 +1,7 @@
 basedir=$1
-cd ${basedir}/target/tmp/aai_schema
-cp `ls -v | tail -1` ${basedir}/target/tmp/aai_schema/aai_schema_latest.xsd || exit 1 
-echo "get-latest-xsd-version.sh has successfully copied aai_schema_latest.xsd to ${basedir}/target/tmp/aai_schema/latest_aai_schema"
+cd ${basedir}/target/aai-schema
+cp `ls -v | tail -1` ${basedir}/target/aai-schema/aai_schema_latest.xsd || exit 1 
+echo "get-latest-xsd-version.sh has successfully copied aai_schema_latest.xsd to ${basedir}/target/aai-schema"
 exit 0
 
 
