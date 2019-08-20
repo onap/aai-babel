a19e97f60175ccf1ec506e34e3952e52d912c383
rajeevme <rajeev.mehta@amdocs.com>
2019-08-20 17:00:16 +0530

    [AAI-2174] Run container process as non-root
    Issue-ID: AAI-2174
    
    Change-Id: I03bb19ef972abcc59f9fad145b715ac143d08f98
    Signed-off-by: rajeevme <rajeev.mehta@amdocs.com>

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -31,6 +31,31 @@ if [ -z "${KEY_STORE_PASSWORD}" ]; then
 	exit 1
 fi
 
+# Changes related to:AAI-2174
+# Change aai babel  container processes to run as non-root on the host
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001}
+
+if [ $(cat /etc/passwd | grep aaiadmin | wc -l) -eq 0 ]; then
+
+        groupadd aaiadmin -g ${GROUP_ID} || {
+                echo "Unable to create the group id for ${GROUP_ID}";
+                exit 1;
+        }
+        useradd --shell=/bin/bash -u ${USER_ID} -g ${GROUP_ID} -o -c "" -m aaiadmin || {
+                echo "Unable to create the user id for ${USER_ID}";
+                exit 1;
+        }
+fi;
+
+chown -R aaiadmin:aaiadmin ${APP_HOME}
+find ${APP_HOME}  -name "*.sh" -exec chmod +x {} +
+
+gosu aaiadmin ln -s /logs $MICRO_HOME/logs
+
+JAVA_CMD="exec gosu aaiadmin java";
+###
+
 PROPS="-DAPP_HOME=${APP_HOME}"
 PROPS="${PROPS} -DCONFIG_HOME=${CONFIG_HOME}"
 PROPS="${PROPS} -Dtosca.mappings.config=${CONFIG_HOME}/tosca-mappings.json"
@@ -41,4 +66,4 @@ fi
 
 JVM_MAX_HEAP=${MAX_HEAP:-1024}
 
-exec java -Xmx${JVM_MAX_HEAP}m ${PROPS} -jar ${APP_HOME}/babel.jar
+${JAVA_CMD}  -Xmx${JVM_MAX_HEAP}m ${PROPS} -jar ${APP_HOME}/babel.jar
